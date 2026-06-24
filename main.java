0c21752f398bfcfaf9b77abaf0153dc98caf3bb4
Eddie Irigoyen <daddyed@MacBook-Pro-9.local>
2026-06-24 14:06:22 -0700

    complete A00

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void Main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("CSCI-19A Object-oriented Programing in java");
+    }
+}
